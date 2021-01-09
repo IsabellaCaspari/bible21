f7d36c18acb599c749cbdf770e4a310e119e9f3d
isabellaCaspari <ica@adorsys.de>
2021-01-09 12:51:48 +0100

    Update Version released in Playstore

--- lib/data/const.dart
+++ lib/data/const.dart
@@ -2,5 +2,5 @@ class Constants {
   static const String BIBLE_PLAN_1="Die komplette Bibel";
   static const String BIBLE_PLAN_2="365 Schlüssel-Kapitel";
   static const String BIBLE_PLAN_3="100 Schlüssel-Texte";
-  static const String APP_VERSION="0.0.2";
+  static const String APP_VERSION="1.0.2";
 }
